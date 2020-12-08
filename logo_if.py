4726f71edadb192a404714a51ed848e95a1489ca
Gabriel <gabrielalves31415@gmail.com>
2020-12-07 23:14:22 -0300

    Organizei o código e escrevi o readme

--- logo_if.py
+++ logo_if.py
@@ -3,13 +3,20 @@ import turtle
 import geometria
 
 class LogoIF:
-    def __init__(self):
-        self.cursor = turtle.Turtle()
-        self.configurar_cursor()
+    def __init__(self, cursor):
+        self.cursor = cursor
         
         self.comprimento_quadrado = 100
         self.cor_borda_quadrado = "green"
         self.cor_fundo_quadrado = "green"
+
+        self.numero_quadrados_primeira_pilha = 3
+        self.numero_quadrados_segunda_pilha = 4
+        self.numero_quadrados_terceira_pilha = 2
+
+        self.intercalar_quadrados_primeira_pilha = False
+        self.intercalar_quadrados_segunda_pilha = False
+        self.intercalar_quadrados_terceira_pilha = True
 
         self.raio_circulo = self.comprimento_quadrado / 2
         self.cor_borda_circulo = "red"
@@ -17,9 +24,9 @@ class LogoIF:
         
         self.distancia_entre_figuras = self.comprimento_quadrado / 10
 
-    def configurar_cursor(self):
-        self.cursor.shape("blank")
-        self.cursor.speed(10)
+        self.posicao_inicial_primeira_pilha_quadrados = (-50, -110)
+        self.posicao_inicial_segunda_pilha_quadrados = (60, 0)
+        self.posicao_inicial_terceira_pilha_quadrados = (170, 0)
 
     def desenhar_logo(self):
         self.desenhar_circulo_da_primeira_pilha()
@@ -31,20 +38,31 @@ class LogoIF:
         geometria.desenhar_circulo(self.cursor, self.raio_circulo, self.cor_borda_circulo, self.cor_fundo_circulo)
 
     def desenhar_primeira_pilha_quadrados(self):
-        self.posicionar_seta_na_base_da_primeira_pilha()
-        self.desenhar_pilha_quadrados(3)
+        self.posicionar_cursor_na_primeira_pilha_quadrados()
+        self.desenhar_pilha_quadrados(self.numero_quadrados_primeira_pilha, self.intercalar_quadrados_primeira_pilha)
 
     def desenhar_segunda_pilha_quadrados(self):
-        self.posicionar_seta_na_base_da_pilha_seguinte()
-        self.desenhar_pilha_quadrados(4)
+        self.posicionar_cursor_na_segunda_pilha_quadrados()
+        self.desenhar_pilha_quadrados(self.numero_quadrados_segunda_pilha, self.intercalar_quadrados_segunda_pilha)
 
     def desenhar_terceira_pilha_quadrados(self):
-        self.posicionar_seta_na_base_da_pilha_seguinte()
-        self.desenhar_pilha_quadrados(2, True)
+        self.posicionar_cursor_na_terceira_pilha_quadrados()
+        self.desenhar_pilha_quadrados(self.numero_quadrados_terceira_pilha, self.intercalar_quadrados_terceira_pilha)
 
-    def posicionar_seta_na_base_da_primeira_pilha(self):
-        self.descer_ate_a_base_do_quadrado_inferior()
-        self.ir_do_ponto_medio_da_base_ate_o_canto_inferior_esquerdo_quadrado()
+    def posicionar_cursor_na_primeira_pilha_quadrados(self):
+        self.cursor.penup()
+        self.cursor.setposition(self.posicao_inicial_primeira_pilha_quadrados)
+        self.cursor.pendown()
+
+    def posicionar_cursor_na_segunda_pilha_quadrados(self):
+        self.cursor.penup()
+        self.cursor.setposition(self.posicao_inicial_segunda_pilha_quadrados)
+        self.cursor.pendown()
+
+    def posicionar_cursor_na_terceira_pilha_quadrados(self):
+        self.cursor.penup()
+        self.cursor.setposition(self.posicao_inicial_terceira_pilha_quadrados)
+        self.cursor.pendown()
 
     def descer_ate_a_base_do_quadrado_inferior(self):
         self.cursor.penup()
@@ -62,33 +80,24 @@ class LogoIF:
         self.cursor.forward(self.comprimento_quadrado)
         self.cursor.left(90)
 
-    def ir_do_ponto_medio_da_base_ate_o_canto_inferior_esquerdo_quadrado(self):
-        self.cursor.penup()
-        self.cursor.right(180)
-        self.cursor.forward(self.comprimento_quadrado / 2)
-        self.cursor.left(180)
-        self.cursor.pendown()
-
-    def posicionar_seta_na_base_da_pilha_seguinte(self):
-        self.cursor.penup()
-        self.cursor.forward(self.comprimento_quadrado + self.distancia_entre_figuras)
-        self.cursor.left(90)
-        self.cursor.forward(3 * self.comprimento_quadrado + 3 * self.distancia_entre_figuras)
-        self.cursor.right(90)
-        self.cursor.pendown()
-
     def desenhar_pilha_quadrados(self, numero_quadrados, intercalar_quadrados = False):
-        for i in range(numero_quadrados):
+        ultimo_quadrado = numero_quadrados - 1
+
+        for quadrado_atual in range(numero_quadrados):
             geometria.desenhar_poligono(self.cursor, self.comprimento_quadrado, 4, self.cor_borda_quadrado, self.cor_fundo_quadrado)
             
-            if i != numero_quadrados - 1:
+            if quadrado_atual != ultimo_quadrado:
                 self.descer_ate_a_base_do_quadrado_inferior()
 
                 if intercalar_quadrados:
                     self.descer_ate_a_base_do_quadrado_inferior()
 
 if __name__ == "__main__":
-    logo_if = LogoIF()
+    cursor = turtle.Turtle()
+    cursor.shape("blank")
+    cursor.speed(10)
+    
+    logo_if = LogoIF(cursor)
     logo_if.desenhar_logo()
 
     turtle.mainloop()
--- readme.md
+++ readme.md
@@ -1,18 +1,23 @@
 # logo-if
 
-Desenha a logo do Instituto Federal(IF) através do módulo *turtle* da linguagem *Python*.
+Desenha a logo do Instituto Federal(IF) através do módulo *turtle* da linguagem *Python*. Eu comecei esse projeto inicialmente como um desafio, mas conforme fui escrevendo o código, acabou se tornando uma homenagem minha ao local onde um dia estudei.
 
 ## Como usar
 
 1. Clone o repositório : `git clone https://github.com/gabr-01/logo-if.git`
-2. Execute o arquivo *logo_if.py*
+2. Execute o arquivo *logo_if.py* e a logo padrão será automaticamente desenhada.
+3. Para instruções de como customizar a logo, veja a seção de screenshots.
 
 ## Screenshots
 
-### A logo desenhada
+### A logo padrão desenhada.
 
 ![screenshot com a logo regular](imagens/screenshot-logo-if-regular.png)
 
-### É possível alterar as cores livremente
+### É possível alterar as cores facilmente, bastando modificar as variáveis *cor_borda_circulo*, *cor_fundo_circulo*, *cor_borda_quadrado* e *cor_fundo_quadrado* presentes no construtor da classe *LogoIF*. Você pode encontrar uma paleta com as strings das cores nesse link : https://trinket.io/docs/colors.
 
 ![screenshot com a logo de cor alterada](imagens/screenshot-logo-if-cor-alterada.png)
+
+### Ainda no método construtor, é possível alterar a quantidade de quadrados em cada pilha, sendo necessário modificar as variáveis *numero_quadrados_primeira_pilha*, *numero_quadrados_segunda_pilha* e *numero_quadrados_terceira_pilha*. Além disso, é possível definir se eles devem ser intercalados ou não setando o booleano nas variáveis *intercalar_quadrados_primeira_pilha* *intercalar_quadrados_segunda_pilha* e *intercalar_terceira_primeira_pilha*
+
+![screenshot com a logo de cor alterada](imagens/screenshot-logo-if-forma-alterada.png)
